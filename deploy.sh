b497127c5f9bc6de5822113d45d5011efb4040b9
otash16 <otajonqutbiddinov1@gmail.com>
2025-08-08 18:24:18 +0500

    feat: deploy sh has been modified

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,8 @@
-# #!/bin/bash
-
-# # PRODUCTION
-# git checkout master
-# git pull origin master
-
-# docker compose up -d
-
-
 #!/bin/bash
 
-# Step 1: Checkout to master and pull latest code
+# PRODUCTION
 git checkout master
 git pull origin master
 
-# Step 2: Stop and remove old containers, volumes, and images
-docker compose down -v --remove-orphans --rmi all
-
-# Step 3: Rebuild everything from scratch
-docker compose build --no-cache
-
-# Step 4: Start containers
 docker compose up -d
+
